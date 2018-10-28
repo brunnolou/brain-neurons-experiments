e8627c4e4b43508b19046cc3795fd3711c7c1548
Bruno Lourenco <brunnolou@gmail.com>
2018-10-28 09:07:38 +0100

    Updates

--- precache-manifest.40a6738414d2c20455a9f555e2895fdb.js
+++ precache-manifest.40a6738414d2c20455a9f555e2895fdb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/brain-neurons-experiments/static/js/runtime~main.0b8d54ee.js"
   },
   {
-    "revision": "d1fc907d6004a5af4c0d",
-    "url": "/brain-neurons-experiments/static/js/main.d1fc907d.chunk.js"
+    "revision": "3fefd70a0f6a73ce3acc",
+    "url": "/brain-neurons-experiments/static/js/main.3fefd70a.chunk.js"
   },
   {
     "revision": "e32de323bd431a53d349",
     "url": "/brain-neurons-experiments/static/js/1.e32de323.chunk.js"
   },
   {
-    "revision": "d1fc907d6004a5af4c0d",
+    "revision": "3fefd70a0f6a73ce3acc",
     "url": "/brain-neurons-experiments/static/css/main.a7b06061.chunk.css"
   },
   {
-    "revision": "4709eab2a59a5ad69eaab4eccf91c13f",
+    "revision": "d1ed5d55339d7e9e4190751e02ded3c4",
     "url": "/brain-neurons-experiments/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/brain-neurons-experiments/precache-manifest.7581dc1ebf506a7fe661cc76cf94d30e.js"
+  "/brain-neurons-experiments/precache-manifest.40a6738414d2c20455a9f555e2895fdb.js"
 );
 
 workbox.clientsClaim();
